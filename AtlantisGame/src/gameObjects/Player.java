94eea67e9b6a4a78ffaae0a4ec907b8c4e64714f
Hyp-nos <ali.habbabeh@hotmail.com>
2016-09-22 22:39:53 +0200

    new changes

--- AtlantisGame/src/gameObjects/Player.java
+++ AtlantisGame/src/gameObjects/Player.java
@@ -1,5 +1,29 @@
 package gameObjects;
 
-public class Player {
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
+public class Player extends VBox{
+	private Label lblName= new Label();
+	
+	private int victoryPoints;
+	
+	private Label lblPlayerImage;
+	
+	private PlayerHand playerHand;
+	
+	
+	
+	
+	public Player(String name){
+		super();  //the player extends vbox 
+		
+		this.getChildren().addAll(lblName,lblPlayerImage);   // i need to add vp as well somehow
+		
+		
+		
+		
+		
+	}			
+							
 }
