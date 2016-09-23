184277a0a42e42d86cbccd9ba72de032cff12c8c
Hyp-nos <ali.habbabeh@hotmail.com>
2016-09-23 09:29:27 +0200

    card class first version

--- AtlantisGame/src/gameObjects/Card.java
+++ AtlantisGame/src/gameObjects/Card.java
@@ -1,6 +1,36 @@
 package gameObjects;
-//Ali
+
+import javafx.scene.paint.Color;
+
+/*
+ * @author Ali Habbabeh
+ */
 
 public class Card {
+	Color blue = Color.BLUE;
+	Color red = Color.RED;
+	Color gray = Color.GRAY;
+	Color yellow = Color.YELLOW;
+	Color green = Color.LIGHTSEAGREEN;
+	Color purple = Color.PURPLE;
+	Color brown = Color.BROWN;
+	
+	public enum ColorChoice { blue,red,gray,yellow,green,purple,brown};
+	
+	private final int cardValue=1;
+	private ColorChoice colorChoice;
+	
+	public Card(ColorChoice colorChoice){
+		
+		this.colorChoice=colorChoice;
+	}
+	
+	public ColorChoice getColor(){
+		return colorChoice;
+	}
+	
+	public int getCardValue(){  // in case we will introduce something like super card this can be changed
+		return 1;
+	}
 
 }
