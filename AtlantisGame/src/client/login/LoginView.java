959dd79161526c6a6a94881c6a9eb80c46957a33
Kevin Neuschwander <kevin.neuschwander@students.fhnw.ch>
2016-09-23 14:24:49 +0200

    a little bit more content

--- AtlantisGame/src/client/login/LoginView.java
+++ AtlantisGame/src/client/login/LoginView.java
@@ -1,19 +1,48 @@
 package client.login;
 
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class LoginView {
-	Stage stage;
-	Scene scene;
-	GridPane root;
+	private Stage stage;
+	private Scene scene;
+	private VBox root;
+	private TitledPane loginPane;
+	private TitledPane createPane;
+	
+	private Label logindescriptionlbl;
+	private Label usernamelbl;
+	private Label passwordlbl;
+	
+	private TextField usernametxt;
+	private PasswordField passwordtxt;
+	
+	private Label createdescriptionlbl;
 	
 	public LoginView()
 	{
 		stage = new Stage();
-		root = new GridPane();
+		
+		logindescriptionlbl = new Label("Enter your name and password");
+		
+		createdescriptionlbl = new Label("Enter your preferred name and password");
+		
+		loginPane = new TitledPane();
+		loginPane.setText("Login");
+		loginPane.setContent(logindescriptionlbl);
+		loginPane.setExpanded(true);
+		
+		createPane = new TitledPane();
+		createPane.setText("Create account");
+		createPane.setContent(createdescriptionlbl);
+		createPane.setExpanded(false);
+		
+		root = new VBox();
+		root.getChildren().addAll(loginPane, createPane);
 		scene = new Scene(root);
+		
 		stage.setScene(scene);
 	}
 	
