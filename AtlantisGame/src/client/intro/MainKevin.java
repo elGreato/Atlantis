e7c1ae887189b60cb365f56ceab07c65a33a365e
Kevin Neuschwander <kevin.neuschwander@students.fhnw.ch>
2016-09-23 13:08:04 +0200

    hope it's my name now

---
+++ AtlantisGame/src/client/intro/MainKevin.java
@@ -0,0 +1,21 @@
+package client.intro;
+
+import gameObjects.Player;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainKevin extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	 @Override
+	 public void start(Stage primaryStage) {
+
+	   
+	}
+
+}
