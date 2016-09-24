4d9e3423ee822121454bc91f50f3d4466f46a202
Kevin Neuschwander <kevin.neuschwander@students.fhnw.ch>
2016-09-24 14:25:32 +0200

    continued with database implementation

--- AtlantisGame/src/server/frontend/ServerModel.java
+++ AtlantisGame/src/server/frontend/ServerModel.java
@@ -7,17 +7,21 @@ import java.net.SocketException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+
 
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import server.backend.Lobby;
 import server.backend.User;
 
 public class ServerModel implements Runnable{
 	
 	private ServerSocket serverSocket;
-	private Lobby lobby = new Lobby();
+	private Lobby lobby;
 	private ServerView view;
 	private Connection con;
 
@@ -37,6 +41,7 @@ public class ServerModel implements Runnable{
 		String username = view.dbusernametxt.getText();
 		String password = view.dbpasswordtxt.getText();
 		
+		//Test if connection to database is possible
 		try {
 			
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/", username, password);
@@ -50,6 +55,40 @@ public class ServerModel implements Runnable{
 			alert.setContentText("Could not connect to database. Check whether username and password are entered correctly and whether there is a mysql server running on the localhost");
 			alert.showAndWait();
 			
+		}
+		
+		//Test if database for atlantis exists
+		
+		try {
+			Statement teststmt = con.createStatement();
+			teststmt.executeUpdate("USE ATLANTISDB;");
+			
+		}
+		catch(SQLException e)
+		{
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setTitle("No instance existing");
+			alert.setContentText("Did not detect an instance of atlantisdb on localhost. Create?");
+			Optional<ButtonType> answer = alert.showAndWait();
+			if(answer.get() == ButtonType.OK)
+			{
+				try {
+					Statement createstmt = con.createStatement();
+					createstmt.executeUpdate("CREATE DATABASE IF NOT EXISTS ATLANTISDB;");
+					createstmt.executeUpdate("USE ATLANTISDB;");
+					createstmt.executeUpdate("CREATE TABLE IF NOT EXISTS users ("
+							 + "username VARCHAR(10) PRIMARY KEY,"
+							 + "userpwd VARCHAR(10),"
+							 + "games_played INT(5),"
+							 + "games_won INT(5),"
+							 + "games_lost INT(5));");
+					
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
 		}
 	}
 	
