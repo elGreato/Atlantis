55f3ecbe89d48c271300c473cc6d1cb5b73645a3
Hyp-nos <ali.habbabeh@hotmail.com>
2016-09-26 22:25:35 +0200

    new classes

---
+++ AtlantisGame/src/gameObjects/MainBoard.java
@@ -0,0 +1,20 @@
+package gameObjects;
+
+
+import java.util.Hashtable;
+
+
+public class MainBoard {
+	private final int numberOfTiles=100;
+	// i chose hashtable and not hashmap cuz it doesn't accept nulls
+	// so in our case here it makes more sense
+	public Hashtable<Integer, WaterTile> initialBoard = new Hashtable<>();
+	
+	public  Hashtable<Integer, WaterTile> createBoard(){
+		for (int i =0; i<numberOfTiles;i++){
+			initialBoard.put(i, new WaterTile(i));
+		}
+		return initialBoard;
+	}
+
+}
