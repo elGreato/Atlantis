54ba415e5926c077568d32e2a0e548ab6e1e233f
Kevin Neuschwander <kevin.neuschwander@students.fhnw.ch>
2016-09-27 22:30:57 +0200

    added one button

--- AtlantisGame/src/client/lobby/LobbyView.java
+++ AtlantisGame/src/client/lobby/LobbyView.java
@@ -6,6 +6,7 @@ import gameObjects.GameListItem;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -23,6 +24,7 @@ public class LobbyView {
 	private GridPane root;
 	private TitledPane createGameSection;
 	private TitledPane joinGameSection;
+	private VBox joinGameSectionContent;
 	private GridPane createGameControls;
 	
 	private Label title;
@@ -34,6 +36,8 @@ public class LobbyView {
 	private TableView leaderBoard;
 	private ObservableList<GameListItem> gameData;
 	
+	private Button joinButton;
+	
 	public LobbyView()
 	{
 		
@@ -43,7 +47,9 @@ public class LobbyView {
 		root = new GridPane();
 		createGameControls = new GridPane();
 		
+		
 		gameList = new TableView<GameListItem>();
+		//Columns for table
 		gameNameCol = new TableColumn("Name");
 		gameNameCol.setResizable(false);
 		playersCol = new TableColumn("Players");
@@ -52,21 +58,29 @@ public class LobbyView {
 		playersRegCol.setResizable(false);
 		playersMaxCol = new TableColumn("Max.");
 		playersMaxCol.setResizable(false);
-
+		
+		//Connect table content to variables
 		gameNameCol.setCellValueFactory(new PropertyValueFactory<GameListItem, String>("gameName"));
 		playersRegCol.setCellValueFactory(new PropertyValueFactory<GameListItem, Integer>("registeredPlayers"));
 		playersMaxCol.setCellValueFactory(new PropertyValueFactory<GameListItem, Integer>("maxPlayers"));
 		
+		//Draw table
 		playersCol.getColumns().addAll(playersRegCol, playersMaxCol);
 		gameList.getColumns().addAll(gameNameCol, playersCol);
 		gameList.setPrefWidth(gameNameCol.getWidth() + playersCol.getWidth()+2);
 		gameList.autosize();
 		gameList.setItems(gameData);
 		
+		//Example entry
 		gameData.add(new GameListItem("ProGame",3,4));
 		
-		joinGameSection = new TitledPane("Join game",gameList);
+		joinButton = new Button("Join game");
+		joinGameSectionContent = new VBox();
+		joinGameSectionContent.getChildren().addAll(gameList, joinButton);
+		joinGameSection = new TitledPane("Join game", joinGameSectionContent);
 		joinGameSection.setExpanded(true);
+		
+		
 		createGameSection = new TitledPane("Create game",createGameControls);
 		createGameSection.setExpanded(true);
 		
