2df7cede9db62d1bb04fcbdf9e5613ee9e13d477
Ali-Lap <Ali-Lap@10.207.7.127>
2016-10-20 09:32:31 +0200

    nothing

--- AtlantisGame/src/client/intro/MainAli.java
+++ AtlantisGame/src/client/intro/MainAli.java
@@ -54,7 +54,7 @@ public class MainAli extends Application {
 	//	root.setTop(p1);
 		
 		root.setBottom(p2);
-		primaryStage.setScene(new Scene(root,3840,2160));
+		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
 		
 		
