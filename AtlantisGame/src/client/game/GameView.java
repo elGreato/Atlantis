c90e927c16e0a5bc14b5fab5616a6664d3083bdd
Kevin Neuschwander <kevin.neuschwander@students.fhnw.ch>
2016-10-24 10:36:02 +0200

    afdsafsdafasfda

--- AtlantisGame/src/client/game/GameView.java
+++ AtlantisGame/src/client/game/GameView.java
@@ -1,5 +1,16 @@
 package client.game;
 
-public class GameView {
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+public class GameView {
+	public GameView()
+	{
+		Stage stage = new Stage();
+		GridPane root = new GridPane();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 }
