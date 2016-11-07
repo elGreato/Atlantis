05ce9adbb7d16e19ef68fb267eb3c99f2321ff0b
Kevin Neuschwander <kevin.neuschwander@students.fhnw.ch>
2016-11-07 12:51:04 +0100

    aa

--- AtlantisGame/src/client/login/LoginView.java
+++ AtlantisGame/src/client/login/LoginView.java
@@ -86,7 +86,7 @@ public class LoginView {
 	public void start()
 	{
 		stage.show();
-		loginusernametxt.requestFocus(); 
+		loginusernametxt.requestFocus();  
 	}
 	public void close() {
 		stage.close();
