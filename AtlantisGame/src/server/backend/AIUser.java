083650db1a59923f91d3ef3672d19b785ffddbc1
Kevin Neuschwander <kevin.neuschwander@students.fhnw.ch>
2016-11-30 18:44:33 +0100

    small change

--- AtlantisGame/src/server/backend/AIUser.java
+++ AtlantisGame/src/server/backend/AIUser.java
@@ -20,7 +20,7 @@ public class AIUser extends User implements Runnable{
 		incomingMessages = new ArrayList<Message>();
 		activeGames = new ArrayList<GameAI>();
 		switch(ui.getUsername()){
-		case "AI_BernGUY" : aiSpeed = -1d; aiPawnSpread = 4d; break;
+		case "AI_BernGUY" : aiSpeed = -1d; aiPawnSpread = 4d; break; 
 		case "AI_Bolt" : aiSpeed = 10d; aiPawnSpread = -5d; break;
 		case "AI_SGASquad" : aiSpeed = 4d; aiPawnSpread = 15d; break;
 		case "AI_GovOfficials" : aiSpeed = 0.02d; aiPawnSpread = -2d; break;
